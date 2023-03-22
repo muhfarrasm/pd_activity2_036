4130a384d8471dc4f3577b710e6cfe2294cafed3
muhfarrasm <muhammadfarrasm@gmail.com>
2023-03-22 17:50:18 +0700

    membuat menu pilihan

--- Swithcase/Swithcase.cpp
+++ Swithcase/Swithcase.cpp
@@ -10,3 +10,19 @@ float luasSegitiga(float a, float t) {
 float luasLingkaran(float r) {
     return 3.14 * r * r;
 }
+int main() {
+    float panjang, lebar, tinggi, jejari, alas;
+    int pilihan;
+
+    cout << "=================";
+    cout << "\n=====M E N U=====";
+    cout << "\n=================";
+
+    cout << "\n1 Luas Perseggi Panjang" << endl;
+    cout << "2 Luas Segitiga" << endl;
+    cout << "3 Luas Lingkaran" << endl;
+    cout << "4 Exit" << endl;
+
+    cout << "Masukan Pilihan" << endl;
+    cout << "\nPilihan (1/2/3/4) : ";
+    cin >> pilihan;
