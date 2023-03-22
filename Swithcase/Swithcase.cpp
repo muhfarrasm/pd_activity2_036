caa12525f5ba1f8d881c5d9b6b36b3eaa44b909c
muhfarrasm <muhammadfarrasm@gmail.com>
2023-03-22 17:53:47 +0700

    membuat Swithcase

--- Swithcase/Swithcase.cpp
+++ Swithcase/Swithcase.cpp
@@ -13,16 +13,54 @@ float luasLingkaran(float r) {
 int main() {
     float panjang, lebar, tinggi, jejari, alas;
     int pilihan;
+    do
+    {
 
-    cout << "=================";
-    cout << "\n=====M E N U=====";
-    cout << "\n=================";
 
-    cout << "\n1 Luas Perseggi Panjang" << endl;
-    cout << "2 Luas Segitiga" << endl;
-    cout << "3 Luas Lingkaran" << endl;
-    cout << "4 Exit" << endl;
+        cout << "=================";
+        cout << "\n=====M E N U=====";
+        cout << "\n=================";
 
-    cout << "Masukan Pilihan" << endl;
-    cout << "\nPilihan (1/2/3/4) : ";
-    cin >> pilihan;
+        cout << "\n1 Luas Perseggi Panjang" << endl;
+        cout << "2 Luas Segitiga" << endl;
+        cout << "3 Luas Lingkaran" << endl;
+        cout << "4 Exit" << endl;
+
+        cout << "Masukan Pilihan" << endl;
+        cout << "\nPilihan (1/2/3/4) : ";
+        cin >> pilihan;
+
+
+		switch (pilihan) {
+		case 1:
+			cout << "Masukkan Panjang : ";
+			cin >> panjang;
+			cout << "Masukkan Lebar : ";
+			cin >> lebar;
+			cout << "Luas Persegi Panjang : " << luasPersegiPanjang(panjang, lebar);
+			break;
+
+		case 2:
+			cout << "Masukkan Alas : ";
+			cin >> alas;
+			cout << "Masukkan Tinggi : ";
+			cin >> tinggi;
+			cout << "Luas Segitiga : " << luasSegitiga(alas, tinggi);
+			break;
+
+		case 3:
+			cout << "Masukkan Jari-Jari : ";
+			cin >> jejari;
+			cout << "Luas Lingkaran : " << luasLingkaran(jejari);
+			break;
+
+		case 4:
+			break;
+
+		default:
+			cout << "Pilihan Salah";
+			break;
+		}
+	} while (pilihan != 4);
+}
+    
